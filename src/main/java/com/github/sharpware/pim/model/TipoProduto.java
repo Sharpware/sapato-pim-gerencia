5e0a00c8eb0e0586509a6cfb898a23e0591ed4d8
Rodrigo Felipe cardoso <rodrigo.fcardoso@hotmail.com>
2015-10-23 17:09:39 -0200

    criação enum TipoProduto

---
+++ src/main/java/com/github/sharpware/pim/model/TipoProduto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.sharpware.pim.model;
+
+/**
+ *
+ * @author RodrigoFelipe
+ */
+public enum TipoProduto {
+    CORRIDA_CAMINHADA("CorridaCaminhada"),
+    CORRIDA_PERFORMACE("CorridaPerformace"),
+    SKATE("Skate"),
+    SURF("Surf"),
+    CHUTEIRA_SALAO("ChuteiraSalao"),
+    CHUTEIRA_CAMPO("ChuteiraCampo"),
+    VOLEI("Volei"),
+    FITNESS("Fitness"),
+    BASQUETE("Basquete");
+    
+    private String nome;
+    
+    TipoProduto(String nome) {
+        this.nome = nome;
+    }
+}
