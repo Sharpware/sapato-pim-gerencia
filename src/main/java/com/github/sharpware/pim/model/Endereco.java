bc7e09a453e3418cf369ffb3efe9b502e60f6637
Rodrigo Felipe cardoso <rodrigo.fcardoso@hotmail.com>
2015-10-23 16:47:17 -0200

    classe Endereco -. Entity

--- src/main/java/com/github/sharpware/pim/model/Endereco.java
+++ src/main/java/com/github/sharpware/pim/model/Endereco.java
@@ -1,14 +1,33 @@
 package com.github.sharpware.pim.model;
 
+import javax.persistence.*;
+
 public class Endereco {
     
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
     private long id;
+    
+    @Column(name="logradouro")
     private String logradouro;
+    
+    @Column(name="numero")
     private int numero;
+    
+    @Column(name="cep")
     private String cep;
+    
+    @Column(name="complemento")
     private String complemento;
+    
+    @Column(name="bairro")
     private String bairro;
+    
+    @Column(name="cidade")
     private String cidade;
+    
+    @Column(name="uf")
     private String uf;
 
     public long getId() {
